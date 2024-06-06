515e6651a72e3648d6507aea417e2a9482ead5c3
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-06-07 01:48:09 +0200

    added overlay text on images

--- Main.js
+++ Main.js
@@ -1,18 +1,20 @@
-
+// @ts-nocheck
 document.addEventListener("DOMContentLoaded", function() {
     var grids = document.querySelectorAll('.grid');
-   grids.forEach((grid)=>{
-    var readMore = grid.querySelector('.read-more');
 
-    grid.addEventListener('mouseenter',()=>{
-        readMore.style.display="block"
-        
-    })
-    grid.addEventListener('mouseleave',()=>{
-        readMore.style.display='none'
-    })
-   })
- 
-    
-})
-  
+    grids.forEach((grid) => {
+        var readMore = grid.querySelector('.read-more');
+
+        grid.addEventListener('mouseenter', () => {
+            if (window.innerWidth > 768) { 
+                readMore.style.display = "block";
+            }
+        });
+
+        grid.addEventListener('mouseleave', () => {
+            if (window.innerWidth > 768) { 
+                readMore.style.display = 'none';
+            }
+        });
+    });
+});
