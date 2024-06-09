4a954ec939d4564db98395739f71e6f0c4c1e6e9
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-06-09 11:25:19 +0200

    change some responsiveness

--- Main.js
+++ Main.js
@@ -1,20 +1,23 @@
 // @ts-nocheck
 document.addEventListener("DOMContentLoaded", function() {
     var grids = document.querySelectorAll('.grid');
+    
 
     grids.forEach((grid) => {
-        var readMore = grid.querySelector('.read-more');
-
+        const readMore = grid.querySelector('.read-more');
         grid.addEventListener('mouseenter', () => {
             if (window.innerWidth > 768) { 
                 readMore.style.display = "block";
+               
             }
         });
-
-        grid.addEventListener('mouseleave', () => {
+       grid.addEventListener('mouseleave', () => {
             if (window.innerWidth > 768) { 
                 readMore.style.display = 'none';
+               
             }
         });
     });
 });
+
+
